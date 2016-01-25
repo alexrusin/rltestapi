c755ccd675b556220162d26ba3e4d44ecd01b5b2
Alex Rusin <alexrusin_2000@yahoo.com>
2016-01-25 15:24:30 -0800

    Add randomizer-slicer function.  Made more questions

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -10,6 +10,7 @@
 
 			$http.get('quiz_data.json').then(function(quizData){
 				$scope.myQuestions = quizData.data;
+				$scope.myQuestions = shuffleSlice($scope.myQuestions);
 				$scope.totalQuestions = $scope.myQuestions.length;
 			});
 
@@ -59,6 +60,25 @@
 				return $sce.trustAsHtml(newMarkup);
 
 			}	
+
+			function shuffleSlice(array) {
+  				var currentIndex = array.length, temporaryValue, randomIndex;
+
+  				// While there remain elements to shuffle...
+  				while (0 !== currentIndex) {
+
+    			// Pick a remaining element...
+			    randomIndex = Math.floor(Math.random() * currentIndex);
+			    currentIndex -= 1;
+
+			    // And swap it with the current element.
+			    temporaryValue = array[currentIndex];
+			    array[currentIndex] = array[randomIndex];
+			    array[randomIndex] = temporaryValue;
+			  	}
+
+			  return array.slice(0, 14);
+			}
 
 
 		}]);
